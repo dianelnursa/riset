0299c21923d9fafc5167fc2200b940c2419cef64
Dian Elnursa <85501002+dianelnursa@users.noreply.github.com>
2023-11-11 00:13:58 +0700

    Update apps.py

--- apps.py
+++ apps.py
@@ -34,7 +34,7 @@ def allowed_file(filename):
 # routes
 @app.route("/developer", methods=['GET', 'POST'])
 def developer():
-	developer_info = {'name': 'Your Name', 'photo_url': '/static/path/to/your/photo.jpg',
+	developer_info = {'name': 'Dian Budi Elnursa', 'photo_url': '/static/path/to/your/photo.jpg',
 	}
 	return render_template("developer.html", developer_info=developer_info)
 	
