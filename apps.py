479a4aebd4e524b45bd73e4cf170b4cc7f661252
Dian Elnursa <85501002+dianelnursa@users.noreply.github.com>
2023-11-11 08:58:40 +0700

    Update apps.py

--- apps.py
+++ apps.py
@@ -32,20 +32,13 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # routes
-@app.route("/developer", methods=['GET', 'POST'])
-def developer():
-	developer_info = {'name': 'Dian Budi Elnursa', 'photo_url': '/static/pestectionadmin/images/photo.jpg','linkedin': 'https://www.linkedin.com/in/dian-budi-e-326760131?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
-    'youtube': 'https://youtube.com/@060_dianelnursa4?si=SYTWDA8lABWj8pSm'}
-	return render_template("developer.html", developer_info=developer_info)
-	
-#@app.route("/", methods=['GET', 'POST'])
-#def main():
-	#return render_template("cnn.html")
-	
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("cnn.html")
+
 @app.route("/classification", methods = ['GET', 'POST'])
 def classification():
 	return render_template("classifications.html")
-
 
 @app.route('/submit', methods=['POST'])
 def predict():
